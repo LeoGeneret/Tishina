723940da18778c9cfb1b820e75cfeecc10b67315
LeoGeneret <lpgeneret@gmail.com>
2020-12-02 23:36:27 +0100

    Finish UI + dialogues Bas Village

---
+++ Assets/Scripts/interaction/Dialogues.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Dialogues : MonoBehaviour
+{
+    public GameObject Dialogue;
+    public GameObject DialogueOverlay;
+    public float duration = 2f;
+    public bool repeteable = false; 
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("collider");
+        Dialogue.SetActive(true);
+        DialogueOverlay.SetActive(true);
+
+        StartCoroutine(PopupDuration());
+    }
+
+    IEnumerator PopupDuration()
+    {
+        yield return new WaitForSeconds(duration);
+
+        DialogueOverlay.SetActive(false);
+        Dialogue.SetActive(false);
+
+
+        if(!repeteable)
+        {
+        transform.GetComponent<Collider>().enabled = false;
+        }
+    }
+}
