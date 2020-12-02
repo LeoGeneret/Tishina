482df56b1677d3dd6255680d915be40f915c1895
LeoGeneret <lpgeneret@gmail.com>
2020-12-02 10:31:07 +0100

    game changer + add shader recolor

---
+++ Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    public void toScene2()
+    {
+        Debug.Log("change scene");
+        SceneManager.LoadScene("VideoIntro");
+    }
+
+    public void toLvl1()
+    {
+        Debug.Log("change scene");
+        SceneManager.LoadScene("1_Basse_Cour");
+    }
+}
