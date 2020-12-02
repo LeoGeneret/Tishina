c25c5585ee3b6c57defdfc52f613eb40e266c548
LeoGeneret <lpgeneret@gmail.com>
2020-12-02 12:40:04 +0100

    Popup done

---
+++ Assets/Scripts/Popup.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Popup : MonoBehaviour
+{
+    public GameObject popUp;
+    public GameObject iconWhenClose;
+    public GameObject iconWhenOpen;
+
+
+    public void show()
+    {
+        popUp.SetActive(true);
+        iconWhenClose.SetActive(false);
+        iconWhenOpen.SetActive(true);
+    }
+
+    public void hide()
+    {
+        popUp.SetActive(false);
+        iconWhenClose.SetActive(true);
+        iconWhenOpen.SetActive(false);
+    }
+}
