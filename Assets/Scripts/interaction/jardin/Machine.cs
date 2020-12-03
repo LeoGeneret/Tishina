d5339b9c83582ef6c834391d24eede3f8b6b9d8a
samyyahia <syahia93250@yahoo.com>
2020-12-03 02:45:40 +0100

    Up Machine Animation

---
+++ Assets/Scripts/interaction/jardin/Machine.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Machine : Interactable
+{
+    public Animator machine;
+
+    // Start is called before the first frame update
+    protected new void Start()
+    {
+        base.Start();
+    }
+    void UpdateMachine()
+    {
+        int destroyHash = Animator.StringToHash("destroy");
+        machine.SetTrigger(destroyHash);
+    }
+
+    public override void Interact()
+    {
+        UpdateMachine();
+    }
+}
