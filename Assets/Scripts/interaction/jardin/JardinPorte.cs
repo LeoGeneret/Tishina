887adc99c724cbddfab3130dd938a058c8367a7e
samyyahia <syahia93250@yahoo.com>
2020-12-03 02:26:48 +0100

    Up

--- Assets/Scripts/interaction/jardin/JardinPorte.cs
+++ Assets/Scripts/interaction/jardin/JardinPorte.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class JardinPorte : Interactable
 {
     public GameObject porteMesh;
+    public GameObject aura;
 
     public float smoothTime = 1.3f;
     public float endPos = 135f;
@@ -17,10 +18,22 @@ public class JardinPorte : Interactable
     void UpdateDoor()
     {
         StartCoroutine(Rotate(Vector3.up, endPos, smoothTime));
+        StartCoroutine(Scale(aura, new Vector3(0, 0, 0), smoothTime / 3));
 
         porteMesh.GetComponent<Collider>().enabled = false;
     }
-
+    IEnumerator Scale(GameObject objectToScale, Vector3 scaleTo, float seconds)
+    {
+        float elapsedTime = 0;
+        Vector3 startingScale = objectToScale.transform.localScale;
+        while (elapsedTime < seconds)
+        {
+            objectToScale.transform.localScale = Vector3.Lerp(startingScale, scaleTo, (elapsedTime / seconds));
+            elapsedTime += Time.deltaTime;
+            yield return new WaitForEndOfFrame();
+        }
+        objectToScale.transform.position = scaleTo;
+    }
     IEnumerator Rotate(Vector3 axis, float angle, float duration = 1.0f)
     {
         Quaternion from = transform.rotation;
